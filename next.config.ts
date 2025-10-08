4014ef4af6f512057fe4c1f6478fd8eca642056c
masaki_yoneno <masaki_yoneno@CoorsTek.com>
2025-10-08 10:12:17 +0900

    prisma setting

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  //画像を扱う場合追加する
+  images: {
+    remotePatterns: [
+    {
+    protocol: 'https',
+    hostname: 'picsum.photos',
+    },
+    ],
+    }
 };
 
 export default nextConfig;
